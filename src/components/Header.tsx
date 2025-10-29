6f1354d8317c541b594adf3153631427c0a98368
abijoserah <serahabijo94@gmail.com>
2025-10-29 12:49:59 +0100

    Mood Player

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
         <div className="headerDiv">
             <h1 className="headerText">MOOD <span className="headerTextSpan">PLAYER</span></h1>
             <p className="headerP">FIND PERFECT TRACKS THAT RESONATE WITH YOUR MOOD AND ELEVATE YOUR SPIRIT</p>
-            <img src="/bouton-de-lecture.png" className="headerIcon" />
+            <img src={`${import.meta.env.BASE_URL}bouton-de-lecture.png`} className="headerIcon" />
         </div>
     )
 }
